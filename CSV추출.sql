abe927b907677d3bd2b4b933104fea736405700c
tmdtp314 <79130369+tmdtp314@users.noreply.github.com>
2021-11-02 09:45:34 +0900

    CSV추출

---
+++ CSV추출.sql
@@ -0,0 +1,33 @@
+-----------------------------------------------------------------------
+ -- xp_cmdshell활성화 
+EXEC sp_configure 'show advanced options', 1
+GO
+
+RECONFIGURE
+GO
+
+EXEC sp_configure 'xp_cmdshell', 1
+GO
+
+RECONFIGURE
+GO
+
+----------------------------------------------------------------------
+-- xp_cmdshell 비활성화
+
+EXEC sp_configure 'xp_cmdshell', 0
+GO
+
+RECONFIGURE
+GO
+
+EXEC sp_configure 'show advanced options', 0
+GO
+
+RECONFIGURE
+GO
+----------------------------------------------------------------------
+
+DECLARE @query VARCHAR(MAX)
+SET @query = 'BCP "SELECT * FROM NSZ04503.dbo.T_QCD" queryout "C:\SQL_tables.csv" -c -t, -U sa -P !h2876633'
+EXEC MASTER..xp_cmdshell @query
