99173fcf77d8fea2e1ce9814e95ab757041fd82c
tmdtp314 <79130369+tmdtp314@users.noreply.github.com>
2021-11-03 17:14:18 +0900

    CHECK조건 추가 한 임시 테이블 활용함수

--- substring_@tbl.sql
+++ substring_@tbl.sql
@@ -21,15 +21,13 @@ END
 
 
 
- -----------------------------------------------------------------------------------
- DECLARE @RUN_01 VARCHAR(MAX) 
+ ------------------------------------------------------------------------------------------------------
+DECLARE @RUN_01 VARCHAR(MAX) 
 DECLARE @TMPTBL TABLE ([RUN_01] VARCHAR(20), CHECK(LEN(RUN_01)>30)) -- 30이 넘으면 CHECK할 거임 
 DECLARE @i INT 
 DECLARE @j INT
 DECLARE @k INT
 SET @RUN_01 = 'R210831-008R210831-007R210831-009R210831-010'
-
-
 
 SET @i = 1 
 SET @j= LEN(@RUN_01)/11
@@ -50,4 +48,8 @@ BEGIN CATCH
  PRINT LEN(@RUN_01)
 END CATCH
 -- SELECT * FROM T_RUN WHERE RUN_01 IN (SELECT RUN_01 FROM @TMPTBL)
+
+ ------------------------------------------------------------------------------------------------------
+
+
 
