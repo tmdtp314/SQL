4fae088b4cc4853b6195143cae68c1b3fd00d643
tmdtp314 <79130369+tmdtp314@users.noreply.github.com>
2021-11-08 14:44:44 +0900

    CTE 재귀문

--- CTE_재귀.sql
+++ CTE_재귀.sql
@@ -40,3 +40,19 @@ WITH CTE(EMPNAME, MGRNAME, DEPT, [LEVEL])
   -- ROOT를 만들기 위해 SUB가 들어간다 
   -- 또 SUB를 만들기 위한 SUB가 있다면 LEVEL이 증가해서 표시
   -- 트리구조 
+
+
+  --------------WITH문 여러번 쓰기-------------------  >> ','로 구분하여 사용!! 
+  WITH CTE(TBL_01, TBL_02)
+        AS (SELECT A_01, A_02 FROM T_A 
+            INTERSECT
+            SELECT B_01, B_02 FROM T_B)
+      ,CTE2(TBL_03,TBL_04)
+        AS (SELECT C_01, C_02 FROM T_C 
+            UNION ALL
+            SELECT D_01, D_02 FROM T_D);
+  
+     SELECT TBL_01,TBL_02 FROM CTE 
+  INTERSECT 
+     SELECT TBL_03,TBL_04 FROM CTE2
+    
